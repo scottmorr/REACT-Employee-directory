d806ce8dcd9b47d66ed48b38cb94e05526ffcb4a
Scott Morrison <scottmorr75@gmail.com>
2020-07-22 23:23:54 -0600

    heroku push

---
+++ client/build/precache-manifest.3cbac4849f74b60614cce9a839bd16fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae6b31e3d04ccdf7fc6c7e69dcac890c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c9ee5dd722c641b2156",
+    "url": "/static/css/main.50b2427e.chunk.css"
+  },
+  {
+    "revision": "6cdb6f262c6eb798be5e",
+    "url": "/static/js/2.f81e1aae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f81e1aae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c9ee5dd722c641b2156",
+    "url": "/static/js/main.ccd6426d.chunk.js"
+  },
+  {
+    "revision": "65bdfdef76476d120aac",
+    "url": "/static/js/runtime-main.dfda8a66.js"
+  }
+]);
